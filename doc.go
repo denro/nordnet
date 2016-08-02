411b2d221866f39cbff94b3e14bd7bbed84b7f35
Dennis Rogenius <denro03@gmail.com>
2016-08-02 09:18:44 +0200

    Make package nordnet instead of relying on separate packages

---
+++ doc.go
@@ -0,0 +1,20 @@
+/*
+	The nordnet package is a set of packages for communicating with the NEXT API.
+
+	The library contains the following packages:
+
+	The api package provides a wrapper to the REST-API.
+
+	The feed package is an implementation for subscribing to the real-time events.
+
+	The util package contans all models used by the packages aswell as a function for generating credentials.
+
+*/
+
+package nordnet
+
+import (
+	_ "github.com/denro/nordnet/api"
+	_ "github.com/denro/nordnet/feed"
+	_ "github.com/denro/nordnet/util"
+)
